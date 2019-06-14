f1360be56da322e4790a23ad78190f35ed5fc518
Guilherme <guilhermebromonschenkel@gmail.com>
2019-06-14 00:09:00 -0300

    Back-end is done

---
+++ back-end/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect("mongodb+srv://guilherme:123@cluster0-cjocp.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes.js'));
+
+server.listen(process.env.PORT || 3333);
